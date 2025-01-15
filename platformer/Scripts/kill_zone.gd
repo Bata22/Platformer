7fafdce7c55804cc6102cb9a39907075bf20639c
Bata22 <nikolic.bratislav123@gmail.com>
2025-01-15 18:34:08 +0100

    Rebuild kill zone respawn point

--- platformer/Scripts/kill_zone.gd
+++ platformer/Scripts/kill_zone.gd
@@ -10,5 +10,5 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	#get_tree().reload_current_scene()
-	get_tree().change_scene_to_file("Bac") 
+	get_tree().reload_current_scene()
+	#get_tree().change_scene_to_file("res://Scenes/game_over.tscn") 
