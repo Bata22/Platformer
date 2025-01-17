217286364aed34bbe03ace09b9a1aacda30b62c9
Bata22 <nikolic.bratislav123@gmail.com>
2025-01-17 21:47:41 +0100

    Added enemy scene, slime new mechanick of dead player

--- platformer/Scripts/kill_zone.gd
+++ platformer/Scripts/kill_zone.gd
@@ -4,11 +4,14 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	timer.start()
-	# Replace with function body.
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
+
 
 
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
 	#get_tree().change_scene_to_file("res://Scenes/game_over.tscn") 
