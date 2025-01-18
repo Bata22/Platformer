477c60a3595b7df4f5c3c01b3d6e6c7f53cd4ab8
Bata22 <nikolic.bratislav123@gmail.com>
2025-01-18 17:00:45 +0100

    Added gameMenager Node for global var score, and funcion for adding points animation for player where is trigerd by movement Changed posittion of a coin

--- platformer/Scripts/coin.gd
+++ platformer/Scripts/coin.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
-var score := 0
+@onready var game_manager: Node = %GameManager
+
+
 
 func _on_body_entered(body: Node2D):
-
+	game_manager.add_point()
 	queue_free()
