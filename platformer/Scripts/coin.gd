d4b4a2454276e23299ed1559538b36c6e75dea85
Bata22 <nikolic.bratislav123@gmail.com>
2025-01-18 18:20:08 +0100

    Added music, wia animation ii done removing coin sprite from screen icon and export feature for .exe file

--- platformer/Scripts/coin.gd
+++ platformer/Scripts/coin.gd
@@ -3,9 +3,8 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 @onready var game_manager: Node = %GameManager
-
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D):
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup")
